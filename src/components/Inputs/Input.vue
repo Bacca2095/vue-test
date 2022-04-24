ccbd1e7b727e0a0781293e6af9b7117cb9630d3f
Cesar Bacca <bacca2095@gmail.com>
2022-04-23 20:02:34 -0500

    feat: agregado componente input

---
+++ src/components/Inputs/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative z-0 mb-6 w-10/12 group mx-auto">
+    <label class="text-md text-gray-300">{{ props.label }}</label>
+    <input
+      class="py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  label: String,
+  placeholder: String,
+  type: String,
+});
+</script>
+
+<style></style>
