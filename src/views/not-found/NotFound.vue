4dad095748d84c706eb4d4d974e3b3380ea77a29
Cesar Bacca <bacca2095@gmail.com>
2022-04-24 01:03:02 -0500

    feat: agregado texto not found

--- src/views/not-found/NotFound.vue
+++ src/views/not-found/NotFound.vue
@@ -1,8 +1,16 @@
 <template>
+  <div class="not-found">
+    <p class="text-6xl">Not Found</p>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <style>
+.not-found {
+  display: grid;
+  width: 100%;
+  height: 100vh;
+  place-items: center;
+}
 </style>
