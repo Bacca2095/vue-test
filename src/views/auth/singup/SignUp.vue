3f6445981c50a3198644a0bfcfddeef9ad1b8edb
Cesar Bacca <bacca2095@gmail.com>
2022-04-24 12:20:01 -0500

    feat: agregado success logo al iniciar sesión

--- src/views/auth/singup/SignUp.vue
+++ src/views/auth/singup/SignUp.vue
@@ -1,13 +1,13 @@
 <template>
   <form class="p-4" @submit.prevent="onSubmit">
-    <Input v-model="v$.firstName.$model" placeholder="John" label="NOMBRE"
-      :invalid="v$.firstName.$dirty ? v$.firstName.$invalid : null" :error="v$.firstName.$errors[0]?.$validator" />
-    <Input v-model="v$.lastName.$model" placeholder="Doe" label="APELLIDO"
-      :invalid="v$.lastName.$dirty ? v$.lastName.$invalid : null" :error="v$.lastName.$errors[0]?.$validator" />
-    <Input v-model="v$.email.$model" placeholder="ejemplo@email.com" label="EMAIL"
-      :invalid="v$.email.$dirty ? v$.email.$invalid : null" :error="v$.email.$errors[0]?.$validator" />
-    <Input v-model="v$.password.$model" placeholder="*******" label="CONTRASEÑA"
-      :invalid="v$.password.$dirty ? v$.password.$invalid : null" :error="v$.password.$errors[0]?.$validator" />
+    <Input v-model="$v.firstName.$model" placeholder="John" label="NOMBRE"
+      :invalid="$v.firstName.$dirty ? $v.firstName.$invalid : null" :error="$v.firstName.$errors[0]?.$validator" />
+    <Input v-model="$v.lastName.$model" placeholder="Doe" label="APELLIDO"
+      :invalid="$v.lastName.$dirty ? $v.lastName.$invalid : null" :error="$v.lastName.$errors[0]?.$validator" />
+    <Input v-model="$v.email.$model" placeholder="ejemplo@email.com" label="EMAIL"
+      :invalid="$v.email.$dirty ? $v.email.$invalid : null" :error="$v.email.$errors[0]?.$validator" />
+    <Input v-model="$v.password.$model" placeholder="*******" label="CONTRASEÑA"
+      :invalid="$v.password.$dirty ? $v.password.$invalid : null" :error="$v.password.$errors[0]?.$validator" />
     <div class="mx-4">
       <Button text="Registro" />
     </div>
@@ -37,7 +37,7 @@ const rules = {
   email: { required, email },
   password: { required },
 };
-const v$ = useVuelidate(rules, {
+const $v = useVuelidate(rules, {
   firstName: toRef(signUpForm, 'firstName'),
   lastName: toRef(signUpForm, 'lastName'),
   email: toRef(signUpForm, 'email'),
@@ -49,12 +49,15 @@ const store = useDefaultStore();
 const router = useRouter();
 
 const onSubmit = async (event: Event) => {
-  const result = await v$.value.$validate();
-  console.log(v$.value.password.$errors[0]?.$validator);
+  const result = await $v.value.$validate();
 
   if (result) {
     store.signIn(signUpForm.email);
-    router.push({ name: RoutesName.HOME });
+    store.$state.showSuccessLogo = true;
+    setTimeout(() => {
+      store.$state.showSuccessLogo = true;
+      router.push({ name: RoutesName.HOME });
+    }, 2000);
   }
 };
 </script>
