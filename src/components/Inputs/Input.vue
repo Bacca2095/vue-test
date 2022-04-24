57bfff8467f48420a6ebe5e4f3480a10fbd22123
Cesar Bacca <bacca2095@gmail.com>
2022-04-24 11:16:48 -0500

    feat: agregada funcionalidad signup
    - Se agrega funcionalidad signup
    - Se agregan props de "invalid" y "error" en componente "Input" para representar el estado de error

--- src/components/Inputs/Input.vue
+++ src/components/Inputs/Input.vue
@@ -3,16 +3,27 @@
     <label class="text-md text-gray-300">{{ props.label }}</label>
     <input :value="modelValue" @input="updateValue" :placeholder="placeholder" :type="type"
       class="py-1.5 px-0 w-full text-sm text-gray-100 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-secondary" />
+    <small v-if="invalid" class="float-right text-secondary">{{
+        error?.toUpperCase()
+    }}</small>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  modelValue: String,
-  label: String,
-  placeholder: String,
-  type: String,
-});
+import { toRefs } from 'vue';
+
+interface InputProps {
+  modelValue: string;
+  label: string;
+  placeholder: string;
+  type: string;
+  invalid: boolean | null;
+  error: string | null;
+}
+
+const props = defineProps<InputProps>();
+
+const { invalid, error } = toRefs(props);
 
 const emit = defineEmits(['update:modelValue']);
 const updateValue = (event: any) => emit('update:modelValue', event.target.value);
