a5e35de70d87e318d4a34833b475cd929ca85489
Cesar Bacca <bacca2095@gmail.com>
2022-04-24 01:01:08 -0500

    feat: agregado componente inicial de singin

--- src/views/auth/singup/SignUp.vue
+++ src/views/auth/singup/SignUp.vue
@@ -1,9 +1,18 @@
 <template>
-    text
+  <form class="p-4">
+    <Input placeholder="John" label="NOMBRE" />
+    <Input placeholder="Doe" label="APELLIDO" />
+    <Input placeholder="ejemplo@email.com" label="EMAIL" />
+    <Input placeholder="*******" label="CONTRASEÑA" />
+    <div class="mx-4">
+      <Button text="Registro" />
+    </div>
+  </form>
 </template>
 
 <script setup lang="ts">
+import Button from '@/components/Buttons/Button.vue';
+import Input from '@/components/Inputs/Input.vue';
 </script>
 
-<style>
-</style>
+<style></style>
